b6a212ab50e6a23142a524d39948f826d0e33dce
bellusfennec <bellusfennec@gmail.com>
2024-02-01 10:51:54 +0300

    refactor:

--- src/components/common/fields/RadioField/index.tsx
+++ src/components/common/fields/RadioField/index.tsx
@@ -1,46 +1,29 @@
 import { InputHTMLAttributes } from "react";
-import { Size, RadioFiledVariant } from "../../../../types";
+import { RadioFiledVariant } from "../../../../types";
 import { toFirstUpperCase } from "../../../../utils/string";
 import style from "./style.module.css";
 
 interface RadioFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-  placeholder?: string;
-  label?: string;
-  description?: string;
-  error?: string;
   required?: boolean;
   variant?: RadioFiledVariant;
-  scale?: Size;
-  disabled?: boolean;
   type?: "radio";
   color?: string;
+  label?: string;
   labelPosition?: "left" | "right";
 }
 
 const RadioField = (props: RadioFieldProps) => {
-  const { label, description, error, required, type = "radio" } = props;
-  const { scale = "md", variant = "filled", color = "blue", labelPosition = "left", ...rest } = props;
+  const { required, type = "radio" } = props;
+  const { label, variant = "filled", color = "#0087ff", labelPosition = "left", ...rest } = props;
 
   return (
-    <div className={style["size" + scale.toUpperCase()]}>
-      {(label || description) && (
-        <div className={style.title}>
-          {label && (
-            <p className={style.label}>
-              {label} {required && <span className={style.required}>*</span>}
-            </p>
-          )}
-          {description && <p className={style.description}>{description}</p>}
-        </div>
-      )}
-      <label
-        className={`${style.container} ${style["variant" + toFirstUpperCase(variant)]}`}
-        style={{ color: `${color}` }}
-      >
-        <input type={type} className={style.input} required={required} {...rest} />
-      </label>
-      {error && <p className={style.error}>{error}</p>}
-    </div>
+    <label
+      className={`${style.container} ${style["variant" + toFirstUpperCase(variant)]}`}
+      style={{ color: `${color}` }}
+    >
+      <input type={type} className={style.input} required={required} {...rest} />
+      {label && <p className={`${style.label} ${style["label" + toFirstUpperCase(labelPosition)]}`}>{label}</p>}
+    </label>
   );
 };
 
--- src/components/ui/SignUp.tsx
+++ src/components/ui/SignUp.tsx
@@ -3,6 +3,7 @@ import { UserSignIn } from "../../types";
 import Form from "../common/Form";
 import RadioField from "../common/fields/RadioField";
 import TextField from "../common/fields/TextField";
+import FieldWrapper from "../common/wrappers/FieldWrapper";
 
 const initialForm = {
   name: "",
@@ -22,8 +23,12 @@ const SignUp = (props: UserSignIn) => {
       <TextField label="Имя" name="name" />
       <TextField label="Ник" name="nickName" startIcon={<p style={{ color: "grey" }}>@</p>} />
       <TextField type="email" label="Email" name="email" />
-      <RadioField type="radio" value="male" name="sex" color="red" />
-      <RadioField type="radio" value="female" name="sex" variant="outline" color="red" />
+      <FieldWrapper label="Пол">
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <RadioField type="radio" value="male" name="sex" label="Мужской" />
+          <RadioField type="radio" value="female" name="sex" label="Женский" />
+        </div>
+      </FieldWrapper>
       <TextField type="password" label="Пароль" name="password" />
       <TextField type="password" label="Повторите пароль" name="passwordRepeat" />
       <button>Зарегестрироватся</button>
